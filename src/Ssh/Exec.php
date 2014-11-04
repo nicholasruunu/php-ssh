6712496f69e69867bd42c67213ec4ead68aca50e
Nicholas Ruunu <nicholas@ruu.nu>
2014-11-04 21:02:41 +0100

    Makes RuntimeError based on error code

--- src/Ssh/Exec.php
+++ src/Ssh/Exec.php
@@ -19,14 +19,31 @@ class Exec extends Subsystem
 
     public function run($cmd, $pty = null, array $env = array(), $width = 80, $height = 25, $width_height_type = SSH2_TERM_UNIT_CHARS)
     {
+        list($output, $error_output) = $this->runRaw($cmd, $pty, $env, $width, $height, $width_height_type);
+        $match = preg_match("/^(.*)\n(0|-?[1-9][0-9]*)$/s", $output, $matches);
+
+        if ( ! $match) {
+            throw new RuntimeException("Output didn't contain return status.");
+        }
+
+        list(, $output, $error_code) = $matches;
+
+        if ($error_code !== "0") {
+            throw new RuntimeException($error_output, (int) $error_code);
+        }
+
+        return $output;
+    }
+
+    private function runRaw($cmd, $pty, $env, $width, $height, $width_height_type) {
+        $cmd .= ';echo -en "\n$?"';
         $stdout = ssh2_exec($this->getResource(), $cmd, $pty, $env, $width, $height, $width_height_type);
         $stderr = ssh2_fetch_stream($stdout, SSH2_STREAM_STDERR);
         stream_set_blocking($stderr, true);
         stream_set_blocking($stdout, true);
-        $error = stream_get_contents($stderr);
-        if ($error !== '') {
-            throw new RuntimeException($error);
-        }
-        return stream_get_contents($stdout);
+        $error_output = stream_get_contents($stderr);
+        $output = stream_get_contents($stdout);
+
+        return array($output, $error_output);
     }
 }
